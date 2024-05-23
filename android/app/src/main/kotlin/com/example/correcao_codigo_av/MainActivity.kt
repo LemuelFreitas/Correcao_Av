54cd256d39b05813e3b61e9b28ca3cd6869e4e7c
LemuelFreitas <lemuel.zuin.freitas@gmail.com>
2024-05-23 11:26:04 -0300

    Correcao_Av

---
+++ android/app/src/main/kotlin/com/example/correcao_codigo_av/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.correcao_codigo_av
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+
+import 'package:correcao_codigo_av/principal.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MaterialApp(
+  debugShowCheckedModeBanner: false,
+  home: Principal(),
+));
+
